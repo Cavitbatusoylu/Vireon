286456960e1bd2b160ff1b48eefb2a2a173fb2fe
EnesKaya <eneskaya5641@gmail.com>
2026-04-12 00:30:07 +0300

    Update

---
+++ Vireon.BusinessLayer/Concrete/TransactionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Vireon.BusinessLayer.Abstract;
+using Vireon.EntityLayer.Concrete;
+
+namespace Vireon.BusinessLayer.Concrete
+{
+    public class TransactionManager : ITransactionService // Sözleşmeyi imzaladık
+    {
+        public void ProcessTransaction(Transaction transaction)
+        {
+            // 1. İŞ KURALI (BUSINESS RULE): Hacker Enes'e Geçit Yok!
+            if (transaction.SenderAccountId == transaction.ReceiverAccountId)
+            {
+                // Eğer gönderen ile alıcı aynı kişiyse, sistemi durdur ve hata fırlat!
+                throw new InvalidOperationException("İşlem hatası: Bir kullanıcı kendi hesabına para gönderemez!");
+            }
+
+            // 2. VERİTABANI KAYDI SİMÜLASYONU
+            Console.WriteLine($"İşlem Başarılı: {transaction.Amount} TL veritabanına işlenmek üzere hazırlanıyor...");
+        }
+    }
+}
