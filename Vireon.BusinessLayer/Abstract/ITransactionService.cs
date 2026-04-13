e66a898038e2af61f470ba48c57d663ee19211ec
Cavit Batu Soylu <CECBSofficial@hotmail.com>
2026-04-14 01:41:29 +0300

    Reapply "Merge remote-tracking branch 'origin/Enes-login'"
    This reverts commit 9b9fcc37713cce27b73f4ae60e41e78fd5c11c29.

---
+++ Vireon.BusinessLayer/Abstract/ITransactionService.cs
@@ -0,0 +1,10 @@
+﻿using Vireon.EntityLayer.Concrete;
+
+namespace Vireon.BusinessLayer.Abstract
+{
+    public interface ITransactionService
+    {
+        // Vezneden gelen temiz veriyi alıp işleyecek olan ana metot
+        void ProcessTransaction(Transaction transaction);
+    }
+}
